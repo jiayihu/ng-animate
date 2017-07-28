c6e65fc0e81aa6f2711957d8f533dff8e8baf22e
Jiayi Hu <jiayi.ghu@gmail.com>
2017-07-28 17:07:39 +0200

    feat: add all attention seekers

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,6 +1,17 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { trigger, transition, useAnimation } from '@angular/animations';
-import { bounce, fade } from '../../src/index';
+import {
+  bounce,
+  flash,
+  pulse,
+  rubberBand,
+  shake,
+  swing,
+  tada,
+  wobble,
+  jello,
+  fade,
+} from '../../src/index';
 
 @Component({
   selector: 'app',
@@ -9,16 +20,38 @@ import { bounce, fade } from '../../src/index';
   encapsulation: ViewEncapsulation.None,
   animations: [
     trigger('bounce', [transition('* => *', [useAnimation(bounce)])]),
+    trigger('flash', [transition('* => *', [useAnimation(flash)])]),
+    trigger('pulse', [transition('* => *', [useAnimation(pulse)])]),
+    trigger('rubberBand', [transition('* => *', [useAnimation(rubberBand)])]),
+    trigger('shake', [transition('* => *', [useAnimation(shake)])]),
+    trigger('swing', [transition('* => *', [useAnimation(swing)])]),
+    trigger('tada', [transition('* => *', [useAnimation(tada)])]),
+    trigger('wobble', [transition('* => *', [useAnimation(wobble)])]),
+    trigger('jello', [transition('* => *', [useAnimation(jello)])]),
 
     trigger('fadeOut', [transition('* => *', [useAnimation(fade)])]),
     trigger('fadeIn', [transition('* => *', [useAnimation(fade)])]),
   ],
 })
 export class AppComponent {
-  bounce: boolean;
+  attentionSeekers = [
+    'bounce',
+    'flash',
+    'pulse',
+    'rubberBand',
+    'shake',
+    'swing',
+    'tada',
+    'wobble',
+    'jello',
+  ];
 
   fadeOut: boolean;
   fadeIn: boolean;
+
+  constructor() {
+    this.attentionSeekers.forEach(name => (this[name] = false));
+  }
 
   animate(name: 'string') {
     this[name] = !this[name];
